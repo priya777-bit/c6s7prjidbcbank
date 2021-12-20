5a5c67f43ae46e1afa366d8e5a488e2ffcda6890
twinkledahale98@gmail.com <twinkledahale98@gmail.com>
2021-12-20 08:54:38 +0530

    idbc

--- c6s7prj-idbcbank/src/DbOperation.java
+++ c6s7prj-idbcbank/src/DbOperation.java
@@ -1,17 +1,45 @@
 import java.sql.*;
+import java.text.ParseException;
+import java.time.*;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 public class DbOperation
 {
-    public String createAccount(String customer_name, Date dob, int customer_phoNo,
-                                 String customer_email, String customer_add,String account_type,
-                                   int account_balance, Date account_open_date,Date transaction_date,
-                                   String transaction_type,String transaction_mode
-                                    ,int transaction_balance)
+    public boolean checkEligibility(Date customer_dob) throws ParseException
+    {
+        boolean result = false;
+        int age;
+
+        Instant instant = customer_dob.toInstant();
+        ZonedDateTime zone = instant.atZone(ZoneId.systemDefault());
+
+        LocalDate localdate = zone.toLocalDate();
+
+        Period period = Period.between(localdate,LocalDate.now());
+
+        age=period.getYears();
+
+        if(age>=18)
+        {
+            System.out.println("Your Eligible To Create Account..");
+            result = true;
+        }
+        return result;
+    }
+
+    public List<Object> createAccount(String customer_name, Date dob, int customer_phoNo,
+                                      String customer_email, String customer_add, String account_type,
+                                      String transaction_type, String transaction_mode)
     {
         int customer_id=0;
         long account_no=0;
         int transaction_id=0;
+
+        int result1=0;
+        long result2=0;
+        int result3=0;
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -33,43 +61,34 @@ public class DbOperation
                 ResultSet rs = pst.getGeneratedKeys();
                 if (rs.next()) { // customer records inserted successfully (customerid)
                      customer_id = rs.getInt(1);
-                    System.out.println(customer_id+":"+rs.getString(2)+":"+rs.getDate(3)
-                            +":"+rs.getInt(4)+":"+rs.getString(5)+":"
-                            +rs.getString(6));
+                     result1=customer_id;
 
-                    String query2 = "insert into account(account_type,account_balance,account_open_date,customer_id)"+
-                            "values(?,?,?,?)";
+                    String query2 = "insert into account(account_type,customer_id)"+
+                            "values(?,?)";
                     PreparedStatement pst2 = con.prepareStatement(query2,Statement.RETURN_GENERATED_KEYS);
                     pst2.setString(1,account_type);
-                    pst2.setInt(2,account_balance);
-                    pst2.setDate(3,new java.sql.Date(account_open_date.getTime()));
-                    pst2.setInt(4,customer_id);
+                    pst2.setInt(2,customer_id);
                     if((pst2.executeUpdate())==1)
                     {
                         ResultSet rs2 = pst2.getGeneratedKeys();
                         if(rs2.next())
                         {
                             account_no = rs2.getInt(1);
-                            System.out.println(account_no+":"+rs2.getString(2)+":"+rs2.getInt(3)
-                            +":"+rs2.getDate(4)+":"+rs2.getInt(5));
+                            result2=account_no;
 
-                            String query3 = "insert into transaction(transaction_date,transaction_type,transaction_mode," +
-                                    "transaction_balance,account_no) values(?,?,?,?,?)";
+                            String query3 = "insert into transaction(transaction_type,transaction_mode," +
+                                    "account_no) values(?,?,?)";
                             PreparedStatement pst3 = con.prepareStatement(query3,Statement.RETURN_GENERATED_KEYS);
-                            pst3.setDate(1,new java.sql.Date(transaction_date.getTime()));
-                            pst3.setString(2,transaction_type);
-                            pst3.setString(3,transaction_mode);
-                            pst3.setInt(4,transaction_balance);
-                            pst3.setLong(5,account_no);
+                            pst3.setString(1,transaction_type);
+                            pst3.setString(2,transaction_mode);
+                            pst3.setLong(3,account_no);
                             if((pst3.executeUpdate())==1)
                             {
                                 ResultSet rs3 = pst3.getGeneratedKeys();
                                 if(rs3.next())
                                 {
                                     transaction_id = rs3.getInt(1);
-                                    System.out.println(transaction_id+":"+rs3.getDate(2)+":"+
-                                            rs3.getString(3)+":"+rs3.getString(4)+":"+
-                                            rs3.getInt(5)+":"+rs.getLong(6));
+                                    result3=transaction_id;
                                 }
                             }
                         }
@@ -84,7 +103,159 @@ public class DbOperation
         catch (Exception e) {
             System.out.println(e);
         }
-        return customer_id+":"+account_no+":"+transaction_id; // custoid, accid, trid
+        return Arrays.asList(result1,result2,result3); // custoid, accid, trid
+    }
+
+    public boolean withdraw(long account_no,int amount_withdraw)
+    {
+        boolean result = false;
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/IDBC", "root",
+                    "root");
+            System.out.println("Connection Success..");
+
+            String query = "select account_balance from account where account_no=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setLong(1,account_no);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next())
+            {
+                long account_no1=rs.getLong(1);
+                int account_balance1=rs.getInt(3);
+                System.out.println("Account Number "+account_no1+" Has Account balance "+account_balance1);
+                account_balance1=account_balance1-amount_withdraw;
+                System.out.println("After Withdraw.."+account_balance1+" Withdraw Amount "+amount_withdraw);
+                result=true;
+
+                String query2 = "update account set account_balance=? where account_no=?";
+                PreparedStatement pst2 = con.prepareStatement(query2);
+                pst2.setLong(2,account_no);
+                pst.setInt(1,account_balance1);
+                if((pst.executeUpdate())==1)
+                {
+                    System.out.println("Your Amount Debited..");
+                }
+            }
+
+            String query3 = "select transaction_balance from transaction where account_no=?";
+            PreparedStatement pst3 = con.prepareStatement(query3);
+            pst3.setLong(1,account_no);
+            ResultSet rs2 = pst.executeQuery();
+            while(rs.next())
+            {
+                long account_no2=rs.getLong(6);
+                int transaction_id=rs.getInt(1);
+                System.out.println("Account Number "+account_no2+" has Transaction Id "+transaction_id);
+
+                String query4 = "update transaction set transaction_amount=? where account_no=?";
+                PreparedStatement pst4 = con.prepareStatement(query4);
+                pst4.setLong(2,account_no2);
+                pst4.setInt(1,amount_withdraw);
+                if((pst4.executeUpdate())==1)
+                {
+                    System.out.println("Transaction Is Successful..");
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return result;
+    }
+
+    public boolean deposit(long account_no,int amount_deposit)
+    {
+        boolean result = false;
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/IDBC", "root",
+                    "root");
+            System.out.println("Connection Success..");
+
+            String query = "select account_balance from account where account_no=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setLong(1,account_no);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next())
+            {
+                int account_bal1=rs.getInt(1);
+                //int account_balance1=rs.getInt(3);
+                //System.out.println("Account Number "+account_bal1);
+                account_bal1=account_bal1+amount_deposit;
+                System.out.println("After Deposit Account Balance.."+account_bal1+" Deposited Amount "+amount_deposit);
+                result=true;
+
+                String query2 = "update account set account_balance=? where account_no=?";
+                PreparedStatement pst2 = con.prepareStatement(query2);
+                pst2.setLong(2,account_no);
+                pst.setInt(1,account_bal1);
+                if((pst.executeUpdate())==1)
+                {
+                    System.out.println("Your Amount Credited..");
+                }
+            }
+
+            String query3 = "select transaction_balance from transaction where account_no=?";
+            PreparedStatement pst3 = con.prepareStatement(query3);
+            pst3.setLong(1,account_no);
+            ResultSet rs2 = pst.executeQuery();
+            while(rs.next())
+            {
+                int transaction_bal=rs.getInt(1);
+                //int transaction_id=rs.getInt(1);
+                System.out.println("After Transaction:");
+
+                String query4 = "update transaction set transaction_amount=? where account_no=?";
+                PreparedStatement pst4 = con.prepareStatement(query4);
+                pst4.setLong(2,account_no);
+                pst4.setInt(1,amount_deposit);
+                if((pst4.executeUpdate())==1)
+                {
+                    System.out.println("Transaction Is Successful..");
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return result;
+    }
+
+    public boolean checkBalance(long account_no)
+    {
+        boolean result = false;
+
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered..");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/IDBC", "root",
+                    "root");
+            System.out.println("Connection Success..");
+
+            String query = "select account_balance from account where account_no=?";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setLong(1,account_no);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next())
+            {
+                long account_bal = rs.getLong(1);
+                //int account_balance = rs.getInt(3);
+                System.out.println("Your Account Balance IS: "+account_bal);
+                result = true;
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return result;
     }
 
 //    public boolean accountNumber( int customer_id)
--- c6s7prj-idbcbank/src/IDBCMain.java
+++ c6s7prj-idbcbank/src/IDBCMain.java
@@ -4,25 +4,99 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class IDBCMain
 {
     public static void main(String args[]){
         DbOperation dbms = new DbOperation();
-        java.util.Date dt = new java.util.Date();
-        SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        String date1 = date.format(dt);
-        try
-        {
-            dt=date.parse(date1);
-        }
-        catch(ParseException pe)
-        {
-            System.out.println(pe);
-        }
-        String result = dbms.createAccount("priya",dt,9999999
-                ,"p@p.gmail.com","nasik","savings",100,dt,dt
-                ,"debit","online",100);
-        System.out.println(result);
+        Scanner sc = new Scanner(System.in);
+        int choice;
+//        java.util.Date dt = new java.util.Date();
+//        SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+//        String date1 = date.format(dt);
+//        try
+//        {
+//            dt=date.parse(date1);
+//        }
+//        catch(ParseException pe)
+//        {
+//            System.out.println(pe);
+//        }
+
+        do {
+            System.out.println("1: Check Eligibility To Create Account");
+            System.out.println("2: Create Account");
+            System.out.println("3: With-Draw Amount");
+            System.out.println("4: Deposit Amount");
+            System.out.println("5: Check Account Balance ");
+
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Your Date Of Birth In (yyyy-MM-dd) Format");
+                    String date = sc.next();
+                    java.util.Date dt = null;
+                    try {
+                        dt = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                        boolean result = dbms.checkEligibility(dt);
+                    } catch (ParseException pe) {
+                        System.out.println(pe);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter Customer Name:");
+                    String customer_name = sc.next();
+                    System.out.println("Enter Customer Date Of Birth Date In (yyyy-MM-dd) Format:");
+                    String date2 = sc.next();
+                    java.util.Date dt2 = null;
+                    try {
+                        dt2 = new SimpleDateFormat("yyyy-MM-dd").parse(date2);
+                        boolean result = dbms.checkEligibility(dt2);
+                    } catch (ParseException pe) {
+                        System.out.println(pe);
+                    }
+                    System.out.println("Enter Customer Phone Number:");
+                    int customer_phoNo = sc.nextInt();
+                    System.out.println("Enter Customer Email:");
+                    String customer_email = sc.next();
+                    System.out.println("Enter Customer Address:");
+                    String customer_add = sc.next();
+                    System.out.println("Enter Account Type:");
+                    String account_type = sc.next();
+                    System.out.println("Enter Transaction Type:");
+                    String transaction_type = sc.next();
+                    System.out.println("Enter Transaction Mode:");
+                    String transaction_mode = sc.next();
+
+                    List<Object> result = dbms.createAccount(customer_name, dt2, customer_phoNo, customer_email,
+                            customer_add, account_type, transaction_type, transaction_mode);
+                    break;
+
+                case 3:
+                    System.out.println("Enter 12 Digit Account Number:");
+                    long account_no = sc.nextLong();
+                    System.out.println("Enter Amount To Withdraw:");
+                    int withdraw_amount = sc.nextInt();
+                    boolean result2 = dbms.withdraw(account_no, withdraw_amount);
+                    break;
+
+                case 4:
+                    System.out.println("Enter Account Number:");
+                    long account_no2 = sc.nextLong();
+                    System.out.println("Enter Amount To Deposit:");
+                    int deposit_amount = sc.nextInt();
+                    boolean result3 = dbms.deposit(account_no2, deposit_amount);
+                    break;
+
+                case 5:
+                    System.out.println("Enter Account Number:");
+                    long account_no3 = sc.nextLong();
+                    boolean result4 = dbms.checkBalance(account_no3);
+                    break;
+            }
+        }while(choice!=6);
     }
 }
